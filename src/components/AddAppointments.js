dd791abcfe808deafe2cdeaac10f259863d381df
AlexMalikov94 <alex.malikov94@gmail.com>
2020-09-29 09:36:03 +0500

    Master1

--- src/components/AddAppointments.js
+++ src/components/AddAppointments.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 class AddAppointments extends Component {
 	render() {
-		return <div>Add appointments!</div>;
+		return <div>Add appointments!<p>Add</p></div>;
 	}
 }
 export default AddAppointments;
