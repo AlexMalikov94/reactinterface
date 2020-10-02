68aa6a46b8eda53c0c3c6def5dba55c218eba2c8
AlexMalikov94 <alex.malikov94@gmail.com>
2020-10-02 11:21:09 +0500

    Update

--- src/components/SearchAppointments.js
+++ src/components/SearchAppointments.js
@@ -2,11 +2,7 @@ import React, {Component} from 'react';
 
 class SearchAppointments extends Component {
 	render() {
-<<<<<<< 23dfd3be8017fde66d73c885dfcffcd14034d3e6
 		return <div>Search appointments!</div>;
-=======
-		return <div>Search appointments<p>Search</p><p>appointments</p></div>;
->>>>>>> Feature 2
 	}
 }
 export default SearchAppointments;
